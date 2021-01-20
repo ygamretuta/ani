df3bf925ed34cbd2120c888fe551278ad9ccf11f
Ygam Retuta <ygam.j.retuta@gmail.com>
2021-01-20 13:44:26 +0800

    write handlers and queries for search page gql

--- src/views/GqlPage/Sections/SectionGql.js
+++ src/views/GqlPage/Sections/SectionGql.js
@@ -1,16 +1,101 @@
-import React from "react";
+import React, {useState} from "react";
+
+import { gql } from '@apollo/client';
+import { useLazyQuery } from '@apollo/client';
 
 import { makeStyles } from "@material-ui/core";
+import Backdrop from "@material-ui/core/Backdrop";
+import CircularProgress from "@material-ui/core/CircularProgress";
 
 import styles from "assets/jss/material-kit-react/views/componentsSections/basicsStyle.js";
 
-const gqlStyles = {
+const useStyles = makeStyles(theme => ({
+  backdrop: {
+    zIndex: theme.zIndex.drawer + 1,
+    color: '#fff'
+  },
+  paginationButton:{
+    marginRight: 10  
+  },
   ...styles
-}
-const useStyles = makeStyles(gqlStyles);
+}));
+
+const GET_ANIMES = gql`
+  query ($id: Int, $page: Int, $perPage: Int, $search: String) {
+    Page(page: $page, perPage: $perPage) {
+      pageInfo {
+        total
+        currentPage
+        lastPage
+        hasNextPage
+        perPage
+      }
+      media (id: $id, type: ANIME, search: $search) {
+        id
+        title {
+            english
+            romaji
+        }
+        startDate{
+            year
+            month
+            day
+        }
+        description
+        averageScore
+        coverImage {
+            extraLarge
+        }
+      }
+    }
+  }`;
 
 export default function SectionGql() {
   const classes = useStyles();
+
+  const [search, setSearch] = useState('');
+  const [currentPage, setCurrentPage] = useState(1);
+  const [lastPage, setLastPage] = useState(1);
+
+  const [getAnimes, {loading, error, data}] = useLazyQuery(GET_ANIMES, {
+    onCompleted: data => {
+      console.log('Last Page');
+      setLastPage(data.Page.pageInfo.lastPage);
+    }
+  });
+
+  function handleCancel() {
+    setSearch('');
+  }
+
+  function handlePageGo(pageNumber) {
+    setCurrentPage(pageNumber);
+
+    getAnimes({
+      variables: {
+        search: search,
+        page: pageNumber,
+        perPage: 6
+      }
+    });
+  }
+
+  function handleSearch() {
+    getAnimes({
+      search: search,
+      perPage: 6
+    });
+  }
+
+  if (loading) {
+    return(
+      <Backdrop className={classes.backrop} open={true}>
+        <CircularProgress color="inherit"/>
+      </Backdrop>
+    );
+  }
+
+  if (error ) return <p>Error!</p>
 
   return(
     <div className={classes.sections}>
